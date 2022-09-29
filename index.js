8a9d2f84afa1f376c5c4fcf90d5abdc162fc6f2f
belladi-sharath-chandra <bsharathchandra9@gmail.com>
2022-09-29 06:15:13 +0000

    react intro

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = (
+  <div>
+    <h1 className="greeting"> Belladi sharath chandra</h1>
+    <p> good to see you here </p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
